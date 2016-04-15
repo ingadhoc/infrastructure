fc0555678833b3bee7789513dee66798a47f3a50
Gary Wallis <ggw@unixservice.com>
2016-04-15 19:02:00 -0400

    added some other server status records

--- infrastructure_base/__openerp__.py
+++ infrastructure_base/__openerp__.py
@@ -1,9 +1,14 @@
 {
     'name': 'Infrastructure Base Module',
-    'version': '8.0.0.0.0',
-    'author': 'ADHOC SA',
+    'version': '8.0.0.0.1',
+    'author': '(C) 2016 Gary Wallis for ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
     'depends': [
         'base',
         ],
-    'data': ['data.xml']
+    'data': ['data.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': True
 }
