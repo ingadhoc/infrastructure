0d8d0786db12c368efad7e897b9ae33ee26e5507
Gary Wallis <ggw@unixservice.com>
2016-04-16 11:13:22 -0400

    added tree views and some data notes

--- cloudmanager/models.py
+++ cloudmanager/models.py
@@ -7,23 +7,23 @@ class Provider(models.Model):
 	_description='Cloud service provider with all API required data'
 	name=fields.Char( required=True,
 			help="Name of the cloud provider organization")
-	ftCreateTemplate=fields.Text( name="ftCreateTemplate", required=False,
+	ftCreateTemplate=fields.Text( string="ftCreateTemplate", required=False,
 			help="REST Create template")
-	ftDeleteTemplate=fields.Text( name="ftDeleteTemplate",required=False,
+	ftDeleteTemplate=fields.Text( string="ftDeleteTemplate",required=False,
 			help="REST Delete template")
-	ftStopTemplate=fields.Text( name="ftStopTemplate", required=False,
+	ftStopTemplate=fields.Text( string="ftStopTemplate", required=False,
 			help="REST Stop template")
-	ftStartTemplate=fields.Text( name="ftStartTemplate", required=False,
+	ftStartTemplate=fields.Text( string="ftStartTemplate", required=False,
 			help="REST Start template")
-	fcMainURL=fields.Char( name="fcMainURL", required=False,
+	fcMainURL=fields.Char( string="fcMainURL", required=False,
 			help="Provider administrative web app URL")
-	fcAPIURL=fields.Char( name="fcAPIURL", required=False,
+	fcAPIURL=fields.Char( string="fcAPIURL", required=False,
 			help="Provider API URL")
-	fcAPIUser=fields.Char( name="fcAPIUser", required=False,
+	fcAPIUser=fields.Char( string="fcAPIUser", required=False,
 			help="API login or username")
-	fcAPIPasswd=fields.Char( name="fcAPIPasswd", required=False,
+	fcAPIPasswd=fields.Char( string="fcAPIPasswd", required=False,
 			help="API password")
-	ftNotes=fields.Text( name="ftNotes", required=False,
+	ftNotes=fields.Text( string="ftNotes", required=False,
 			help="Freeform details regarding this cloud provider")
 
 class MachineType(models.Model):
@@ -31,9 +31,9 @@ class MachineType(models.Model):
 	_description='Cloud service provider server specification shorthand name'
 	name=fields.Char(required=True,
 			help="Provider name of the machine type or droplet size (server specification)")
-	ftNotes=fields.Text(name="ftNotes",required=False,
+	ftNotes=fields.Text( string="ftNotes",required=False,
 			help="Freeform details regarding this machine type, droplet size or server specification")
-	fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,name="fm2oProvider",
+	fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,string="fm2oProvider",
 			help="The provider this machine type is valid for")
 
 class Image(models.Model):
@@ -41,9 +41,9 @@ class Image(models.Model):
 	_description='Cloud service provider OS image name'
 	name=fields.Char(required=True,
 			help="Provider name of the OS image")
-	ftNotes=fields.Text(name="ftNotes",required=False,
+	ftNotes=fields.Text( string="ftNotes",required=False,
 			help="Freeform details regarding this OS image")
-	fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,name="fm2oProvider",
+	fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,string="fm2oProvider",
 			help="The provider this image is valid for")
 
 class Zone(models.Model):
@@ -51,17 +51,17 @@ class Zone(models.Model):
 	_description='Cloud service provider name of service region or zone'
 	name=fields.Char(required=True,
 			help="Provider name of the geographical region or zone that will host the server")
-	ftNotes=fields.Text(name="ftNotes",required=False,
+	ftNotes=fields.Text( string="ftNotes",required=False,
 			help="Freeform details regarding this provider zone or region")
-	fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,name="fm2oProvider",
+	fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,string="fm2oProvider",
 			help="The provider this region is valid for")
 
 class ServerStatus(models.Model):
 	_name='cloudmanager.serverstatus'
 	_description='Cloud Manager server status'
-	name=fields.Char(required=True,
+	name=fields.Char(required=True,readonly=True,
 			help="Server status name")
-	ftNotes=fields.Text(name="ftNotes",required=False,
+	ftNotes=fields.Text( string="ftNotes",required=False,
 			help="Freeform details regarding this server status")
 
 class Server(models.Model):
@@ -69,24 +69,24 @@ class Server(models.Model):
 	_description='The server instance'
 	name=fields.Char(required=True,
 			help="Provider name of the instance/droplet or VM server")
-	fcServerFQDN=fields.Char(name="fcServerFQDN", required=False,
+	fcServerFQDN=fields.Char( string="fcServerFQDN", required=False,
 			help="FQDN DNS name of the instance/droplet or VM server")
-	fcDiskSize=fields.Char(name="fcDiskSize",required=False,
+	fcDiskSize=fields.Char( string="fcDiskSize",required=False,
 			help="Disk size, usually related to fcMachineType, also may include disk type like SSD. For informational purposes only.")
-	fcRamSize=fields.Char(name="fcRamSize",required=False,
+	fcRamSize=fields.Char( string="fcRamSize",required=False,
 			help="Ram size, usually related to fcMachineType for informational purposes only.")
-	fcTimeZone=fields.Char(name="fcTimeZone",required=False,
+	fcTimeZone=fields.Char( string="fcTimeZone",required=False,
 			help="Linux time zone name")
-	ftNotes=fields.Text(name="ftNotes", required=False,
+	ftNotes=fields.Text( string="ftNotes", required=False,
 			help="Freeform details regarding this server")
-	fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,name="fm2oProvider",
+	fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,string="fm2oProvider",
 			help="The provider this server is using")
-	fm2oMachineType=fields.Many2one('cloudmanager.machinetype',required=False,name="fm2oMachineType",
+	fm2oMachineType=fields.Many2one('cloudmanager.machinetype',required=False,string="fm2oMachineType",
 			help="The machine type this server is using")
-	fm2oImage=fields.Many2one('cloudmanager.image',required=False,name="fm2oImage",
+	fm2oImage=fields.Many2one('cloudmanager.image',required=False,string="fm2oImage",
 			help="The OS image this server is using")
-	fm2oZone=fields.Many2one('cloudmanager.zone',required=False,name="fm2oZone",
+	fm2oZone=fields.Many2one('cloudmanager.zone',required=False,string="fm2oZone",
 			help="The zone or geographical region this server is assigned to")
-	fm2oServerStatus=fields.Many2one('cloudmanager.serverstatus',required=False,name="fm2oServerStatus",
+	fm2oServerStatus=fields.Many2one('cloudmanager.serverstatus',required=False,string="fm2oServerStatus",
 			help="The status of the server")
 
