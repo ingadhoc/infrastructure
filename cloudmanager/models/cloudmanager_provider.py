f781ce15bd3da6370c2512b265d6247292625100
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-04-28 10:45:06 -0300

    REF provider

--- cloudmanager/models/cloudmanager_provider.py
+++ cloudmanager/models/cloudmanager_provider.py
@@ -4,25 +4,42 @@ from openerp import models, fields
 
 
 class CloudmanagerProvider(models.Model):
-    _name='cloudmanager.provider'
-    _description='Cloud service provider with all API required data'
-    name=fields.Char( required=True,
-            help="Name of the cloud provider organization")
-    ftCreateTemplate=fields.Text( string="ftCreateTemplate", required=False,
-            help="REST Create template")
-    ftDeleteTemplate=fields.Text( string="ftDeleteTemplate",required=False,
-            help="REST Delete template")
-    ftStopTemplate=fields.Text( string="ftStopTemplate", required=False,
-            help="REST Stop template")
-    ftStartTemplate=fields.Text( string="ftStartTemplate", required=False,
-            help="REST Start template")
-    fcMainURL=fields.Char( string="fcMainURL", required=False,
-            help="Provider administrative web app URL")
-    fcAPIURL=fields.Char( string="fcAPIURL", required=False,
-            help="Provider API URL")
-    fcAPIUser=fields.Char( string="fcAPIUser", required=False,
-            help="API login or username")
-    fcAPIPasswd=fields.Char( string="fcAPIPasswd", required=False,
-            help="API password")
-    ftNotes=fields.Text( string="ftNotes", required=False,
-            help="Freeform details regarding this cloud provider")
+    _name = 'cloudmanager.provider'
+    _description = 'Cloud service provider with all API required data'
+
+    name = fields.Char(
+        required=True,
+        help="Name of the cloud provider organization",
+    )
+    creata_template = fields.Text(
+        help="REST Create template",
+    )
+    delete_template = fields.Text(
+        help="REST Delete template",
+    )
+    stop_template = fields.Text(
+        help="REST Stop template",
+    )
+    start_template = fields.Text(
+        help="REST Start template",
+    )
+    main_url = fields.Char(
+        string="Main URL",
+        help="Provider administrative web app URL",
+    )
+    api_url = fields.Char(
+        string="API URL",
+        help="Provider API URL",
+    )
+    api_user = fields.Char(
+        string="API User",
+        help="API login or username",
+    )
+    # for convention do not strip field names
+    api_password = fields.Char(
+        string="API Password",
+        help="API password",
+    )
+    notes = fields.Text(
+        help="Freeform details regarding this cloud provider",
+    )
