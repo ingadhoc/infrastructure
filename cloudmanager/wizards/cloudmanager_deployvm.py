40435bf6737d08979a1b0f7d42fd3a16d2033987
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-04-28 10:35:55 -0300

    REF deployvm and add missing _view suffix to xml

--- cloudmanager/wizards/cloudmanager_deployvm.py
+++ cloudmanager/wizards/cloudmanager_deployvm.py
@@ -1,30 +1,23 @@
 import logging
-from openerp import models, fields, api
-
+from openerp import models, api
 _logger = logging.getLogger(__name__)
 
 
 class DeployVM(models.TransientModel):
     _name = 'cloudmanager.deployvm'
 
-    def _default_session(self):
-        return self.env['cloudmanager.server'].browse(self._context.get('active_id'))
-
-    server_id = fields.Many2one('cloudmanager.server', string="fcName", required=True, default=_default_session)
-
     @api.multi
     def deployvm(self):
-        #print 'self.env.context', self.env.context
-        #print 'self.env.uid', self.env.uid
-        #print 'self.env.cr', self.env.cr
         active_ids = self.env.context.get('active_ids', [])
         if not active_ids:
             return True
         servers = self.env['cloudmanager.server'].browse(active_ids)
         for server in servers:
-            #call the deploy method  in the ProviderAPI class
-            #validate
-            server.state='draft';
+            # call the deploy method  in the ProviderAPI class
+            # validate
+            server.state = 'draft'
+            # TODO we should remove some of this checks because some of them
+            # are required fields
             if not server.name:
                 return False
             if not server.ftNotes:
@@ -35,7 +28,6 @@ class DeployVM(models.TransientModel):
                 return False
             if not server.fm2oServerStatus:
                 return False
-            server.state='ready';
+            server.state = 'ready'
             _logger.info('server_id %s', server.id)
         return True
-
