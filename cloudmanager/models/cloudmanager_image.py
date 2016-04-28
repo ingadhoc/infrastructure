79ec1a4175d73835587dbca3a3bff6b116e6983b
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-04-28 10:28:16 -0300

    REF cloudmanager image

--- cloudmanager/models/cloudmanager_image.py
+++ cloudmanager/models/cloudmanager_image.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-from openerp.exceptions import ValidationError 
-# from requests import requests
-from string import Template
-import json
+from openerp import models, fields
+# from openerp.exceptions import ValidationError
 
 
 class Image(models.Model):
-    _name='cloudmanager.image'
-    _description='Cloud service provider OS image name'
-    name=fields.Char(required=True,
-            help="Provider name of the OS image")
-    ftNotes=fields.Text( string="ftNotes",required=False,
-            help="Freeform details regarding this OS image")
-    fm2oProvider=fields.Many2one('cloudmanager.provider',required=False,string="fm2oProvider",
-            help="The provider this image is valid for")
-    fcSlug=fields.Text( string="fcSlug",required=False,
-            help="Provider abbreviated name used in templates")
+    _name = 'cloudmanager.image'
+    _description = 'Cloud service provider OS image name'
+
+    name = fields.Char(
+        required=True,
+        help="Provider name of the OS image",
+    )
+    notes = fields.Text(
+        help="Freeform details regarding this OS image",
+    )
+    provider_id = fields.Many2one(
+        'cloudmanager.provider',
+        'Provider',
+        help="The provider this image is valid for",
+    )
+    slug = fields.Text(
+        help="Provider abbreviated name used in templates",
+    )
